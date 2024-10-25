2ac6a0b5b4b4111cac9ee621fd44e6a64f6a676a
JPeMANY <jp.hobuss@gmail.com>
2024-10-25 18:20:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let currentIndex = 0;
 
 function slide(direction) {
 
-    console.log(currentIndex)
   const track = document.querySelector('.carousel-track');
   const slides = document.querySelectorAll('.slide');
   const totalSlides = slides.length;
